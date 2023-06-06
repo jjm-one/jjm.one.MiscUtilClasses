624eb9591346cc42263d6826ea6243c247388421
Jonas Merkle [JJM] <jonas@jjm.one>
2023-06-06 15:19:36 +0200

    added ctor

--- src/jjm.one.MiscUtilClasses/Types/basic/AppInfo.cs
+++ src/jjm.one.MiscUtilClasses/Types/basic/AppInfo.cs
@@ -46,6 +46,42 @@ namespace jjm.one.MiscUtilClasses.Types.basic
             ;
 		}
 
+        /// <summary>
+        /// A constructor for the <see cref="AppInfo"/> class.
+        /// </summary>
+        /// <param name="appName"></param>
+        /// <param name="appVersion"></param>
+        /// <param name="appBuildDate"></param>
+        /// <param name="appBuildTime"></param>
+        /// <param name="appRuntimeEnvironment"></param>
+        public AppInfo(string? appName, string? appVersion, string? appBuildDate, string? appBuildTime, string? appRuntimeEnvironment)
+        {
+            // set app name
+            AppName = (appName is null || string.IsNullOrEmpty(appName)) ?
+                @"unknown" :
+                appName;
+
+            // set app version
+            AppVersion = (appVersion is null || string.IsNullOrEmpty(appVersion)) ?
+                @"unknown" :
+                appVersion;
+
+            // set app build date
+            AppBuildDate = (appBuildDate is null || string.IsNullOrEmpty(appBuildDate)) ?
+                @"unknown" :
+                appBuildDate;
+
+            // set app build time 
+            AppBuildTime = (appBuildTime is null || string.IsNullOrEmpty(appBuildTime)) ?
+                @"unknown" :
+                appBuildTime;
+
+            // set app name
+            AppRuntimeEnvironment = (appRuntimeEnvironment is null || string.IsNullOrEmpty(appRuntimeEnvironment)) ?
+                @"unknown" :
+                appRuntimeEnvironment;
+        }
+
         #endregion
     }
 }
