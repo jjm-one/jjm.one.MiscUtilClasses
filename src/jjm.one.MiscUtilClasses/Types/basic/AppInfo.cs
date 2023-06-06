cef6fd295e146e5f9ceceeb898a1d69a68a72e4e
Jonas Merkle [JJM] <jonas@jjm.one>
2023-06-06 03:12:28 +0200

    adds AppInfo class

---
+++ src/jjm.one.MiscUtilClasses/Types/basic/AppInfo.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace jjm.one.MiscUtilClasses.Types.basic
+{
+    /// <summary>
+    /// This class is a wrapper for basic information about an application.
+    /// </summary>
+    public class AppInfo
+	{
+        #region public members
+
+        /// <summary>
+        /// The name of the app.
+        /// </summary>
+        public string AppName = string.Empty;
+
+        /// <summary>
+        /// The version of the app.
+        /// </summary>
+        public string AppVersion = string.Empty;
+
+        /// <summary>
+        /// The build date of the app.
+        /// </summary>
+        public string AppBuildDate = string.Empty;
+
+        /// <summary>
+        /// The build time of the app.
+        /// </summary>
+        public string AppBuildTime = string.Empty;
+
+        /// <summary>
+        /// The runtime environment in which the app was started.
+        /// </summary>
+        public string AppRuntimeEnvironment = string.Empty;
+
+        #endregion
+
+        #region ctor
+
+        /// <summary>
+        /// Default constructor for the <see cref="AppInfo"/> class.
+        /// </summary>
+        public AppInfo()
+		{
+            ;
+		}
+
+        #endregion
+    }
+}
+
